377569f35ff2c2b4a52c8ccaff57bfa91fec7066
giuseppesciacca <sciaccagi@gmail.com>
2023-05-29 18:05:56 +0200

    [add] admin/index table

--- resources/views/admin/comics/index.blade.php
+++ resources/views/admin/comics/index.blade.php
@@ -2,5 +2,41 @@
 
 
 @section('content')
-<h1>admin / </h1>
+<div class="container bg-light py-3">
+
+    <table class="table table-striped m-0">
+        <thead>
+            <tr>
+                <th scope="col">Title</th>
+                <th scope="col">Description</th>
+                <th scope="col">Thumb</th>
+                <th scope="col">Price</th>
+                <th scope="col">Series</th>
+                <th scope="col">Sale date</th>
+                <th scope="col">Type</th>
+            </tr>
+        </thead>
+
+        <tbody>
+
+            @forelse ($comics as $comic)
+            <tr>
+                <td scope="row">{{$comic['title']}}</td>
+                <td>{{$comic['description']}}</td>
+                <td>{{$comic['thumb']}}</td>
+                <td>{{$comic['price']}}</td>
+                <td>{{$comic['series']}}</td>
+                <td>{{$comic['sale_date']}}</td>
+                <td>{{$comic['type']}}</td>
+            </tr>
+
+            @empty
+            <p>No comics</p>
+        </tbody>
+        @endforelse
+    </table>
+
+
+
+</div>
 @endsection
