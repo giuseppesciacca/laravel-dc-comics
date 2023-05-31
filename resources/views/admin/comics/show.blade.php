8cef9ab6b83d2b698eb022f813bd783665fc9c2a
giuseppesciacca <sciaccagi@gmail.com>
2023-05-31 17:13:27 +0200

    [add] style layout admin/show

--- resources/views/admin/comics/show.blade.php
+++ resources/views/admin/comics/show.blade.php
@@ -3,19 +3,7 @@
 @section('content')
 
 <style>
-    #current_comic {
-        position: relative;
-        bottom: 110%;
-        left: 30%;
-        width: 150px;
-        border: 1px solid white;
-    }
 
-    #banner_blue {
-        width: 100%;
-        height: 80px;
-        background-color: rgba(2, 130, 249, 1);
-    }
 </style>
 
 <div class="container bg-light p-0">
@@ -25,7 +13,7 @@
     </div>
     <!-- /.jumbotron + banner_blue -->
 
-    <div class="container px-3 py-5 position-relative">
+    <div class="container_small px-3 py-5 position-relative">
 
         <div id="current_comic" class="position-absolute">
             <img class="img-fluid" width="100" src="{{$comic->thumb}}" alt="{{$comic->title}}">
@@ -34,40 +22,57 @@
 
         <div class="row">
             <div class="col-12 col-lg-8">
-                <h2 class="py-3">{{$comic->title}}</h2>
+                <h2 class="py-3 text_dark_blue">{{$comic->title}}</h2>
 
-                <div class="bg-success text-white row">
+                <div class="banner_green text-white row">
 
-                    <div class="col-7">
-                        <p class="d-flex justify-content-between">
-                            U.S. Price: {{$comic->price}}
-                            AVAIBLE
+                    <div class="col-9">
+                        <p class="m-0 py-3 d-flex justify-content-between">
+                            <span>
+                                U.S. Price: {{$comic->price}}
+                            </span>
+                            <span>
+                                AVAIBLE
+                            </span>
                         </p>
                     </div>
-                    <div class="col-5">
-                        Check Avaibility
+                    <div class="col-3 avaibility_box">
+                        <p class="m-0 py-3 text-center">Check Avaibility</p>
                     </div>
                 </div>
                 <!-- /green banner -->
-                <p>{{$comic->description}}</p>
+
+                <div class="row">
+                    <div class="col-12 p-0 py-3 description">
+                        <p>{{$comic->description}}</p>
+                    </div>
+                </div>
 
             </div>
 
             <div class="col-12 col-lg-4">
-                PUBBLICITà
+                ADVERTISEMENT
             </div>
 
         </div>
+    </div>
 
+    <div class="bottom_show_page">
+        <div class="container_small">
+            <div class="row row-cols-lg-2 g-3 justify-content-center">
+                <div class="col">
+                    <h2 class="border-bottom py-3 m-0 text_dark_blue">Talent</h2>
 
+                    <p class="border-bottom m-0 py-2"><span class="text_dark_blue">Art by:</span></p>
+                    <p class="border-bottom m-0 py-2"><span class="text_dark_blue">Written by:</span></p>
+                </div>
+                <div class="col">
+                    <h2 class="border-bottom py-3 m-0 text_dark_blue">Specs</h2>
 
-
-
-        <div class="row row-cols-3 p-5 justify-content-center">
-            <div class="col">
-                <p><span>{{$comic->series}}</span> <span>Release on {{$comic->sale_date}}</span></p>
-                <p><strong>Type: {{$comic->type}}</strong></p>
-                <p><strong>Price: {{$comic->price}}</strong></p>
+                    <p class="border-bottom m-0 py-2"> <span class="text_dark_blue">Series:</span> {{$comic->series}}</p>
+                    <p class="border-bottom m-0 py-2"><span class="text_dark_blue">U.S. Price:</span> {{$comic->price}}</p>
+                    <p class="border-bottom m-0 py-2"><span class="text_dark_blue">On Sale Date: </span>{{$comic->sale_date}}</p>
+                </div>
             </div>
         </div>
 
