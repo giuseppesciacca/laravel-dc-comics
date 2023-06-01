922b6812ac1472af22bce276e7351bc4d4c90042
giuseppesciacca <sciaccagi@gmail.com>
2023-06-01 12:14:59 +0200

    [add] msg error in store + change language errors

--- resources/views/admin/comics/create.blade.php
+++ resources/views/admin/comics/create.blade.php
@@ -2,6 +2,16 @@
 
 @section('content')
 
+@if ($errors->any())
+<div class="alert alert-danger">
+    <ul>
+        @foreach ($errors->all() as $error)
+        <li>{{$error}}</li>
+        @endforeach
+    </ul>
+</div>
+@endif
+
 <div class="bg-light py-3">
 
     <div class="container">
