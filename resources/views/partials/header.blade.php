d66b4f5d16856c8b87f6387108452a7793592c40
giuseppesciacca <sciaccagi@gmail.com>
2023-06-01 17:29:32 +0200

    [change] nav style

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,51 +1,57 @@
-<header id="app_header" class="">
+<header id="app_header">
     <div class="container">
-        <div class="row">
 
-            <div class="logo col-12 col-md-5 col-lg-2 d-flex align-items-center">
-                <img class="img-fluid" src="{{ Vite::asset('resources/images/dc-logo.png') }}" alt="">
+        <nav class="navbar navbar-expand-xl p-0">
+
+            <div class="logo navbar-brand">
+                <img class="img" src="{{ Vite::asset('resources/images/dc-logo.png') }}" alt="logo">
             </div>
-            <!-- /header left -->
+            <!-- /nav left -->
 
-            <nav class="navbar navbar-expand col-12 col-md-7 col-lg-10 p-0 justify-content-end">
-                <ul class="nav navbar-nav ">
-                    <li class="nav-item d-flex align-items-center px-lg-2">
-                        <a class="nav-link" aria-current="page" href="#">CHARACTERS</a>
-                    </li>
-                    <li class="nav-item d-flex align-items-center text-center px-lg-2">
-                        <a class="nav-link" aria-current="page" href="/">COMICS</a>
-                    </li>
-                    <li class="nav-item d-flex align-items-center px-lg-2">
-                        <a class="nav-link" aria-current="page" href="#">MOVIE</a>
-                    </li>
-                    <li class="nav-item d-flex align-items-center px-lg-2">
-                        <a class="nav-link" aria-current="page" href="#">TV</a>
-                    </li>
-                    <li class="nav-item d-flex align-items-center px-lg-2">
-                        <a class="nav-link" aria-current="page" href="#">GAMES</a>
-                    </li>
-                    <li class="nav-item d-flex align-items-center px-lg-2">
-                        <a class="nav-link" aria-current="page" href="#">COLLECTIBLES</a>
-                    </li>
-                    <li class="nav-item d-flex align-items-center px-lg-2">
-                        <a class="nav-link" aria-current="page" href="#">VIDEOS</a>
-                    </li>
-                    <li class="nav-item d-flex align-items-center px-lg-2">
-                        <a class="nav-link" aria-current="page" href="#">FANS</a>
-                    </li>
-                    <li class="nav-item d-flex align-items-center px-lg-2">
-                        <a class="nav-link" aria-current="page" href="#">NEWS</a>
-                    </li>
-                    <li class="nav-item d-flex align-items-center px-lg-2">
-                        <a class="nav-link" aria-current="page" href="#">SHOP</a>
-                    </li>
-                    <li class="nav-item d-flex align-items-center px-lg-2">
-                        <a class="nav-link" aria-current="page" href="{{route('comics.index')}}"><span><i class="fa-solid fa-user-gear px-2"></i></span>ADMIN</a>
-                    </li>
-                </ul>
-            </nav>
-            <!-- /header right -->
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
-        </div>
+            <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <ul class="nav navbar-nav ">
+                        <li class="nav-item d-flex align-items-center px-lg-2">
+                            <a class="nav-link" aria-current="page" href="#">CHARACTERS</a>
+                        </li>
+                        <li class="nav-item d-flex align-items-center text-center px-lg-2">
+                            <a class="nav-link" aria-current="page" href="/">COMICS</a>
+                        </li>
+                        <li class="nav-item d-flex align-items-center px-lg-2">
+                            <a class="nav-link" aria-current="page" href="#">MOVIE</a>
+                        </li>
+                        <li class="nav-item d-flex align-items-center px-lg-2">
+                            <a class="nav-link" aria-current="page" href="#">TV</a>
+                        </li>
+                        <li class="nav-item d-flex align-items-center px-lg-2">
+                            <a class="nav-link" aria-current="page" href="#">GAMES</a>
+                        </li>
+                        <li class="nav-item d-flex align-items-center px-lg-2">
+                            <a class="nav-link" aria-current="page" href="#">COLLECTIBLES</a>
+                        </li>
+                        <li class="nav-item d-flex align-items-center px-lg-2">
+                            <a class="nav-link" aria-current="page" href="#">VIDEOS</a>
+                        </li>
+                        <li class="nav-item d-flex align-items-center px-lg-2">
+                            <a class="nav-link" aria-current="page" href="#">FANS</a>
+                        </li>
+                        <li class="nav-item d-flex align-items-center px-lg-2">
+                            <a class="nav-link" aria-current="page" href="#">NEWS</a>
+                        </li>
+                        <li class="nav-item d-flex align-items-center px-lg-2">
+                            <a class="nav-link" aria-current="page" href="#">SHOP</a>
+                        </li>
+                        <li class="nav-item d-flex align-items-center px-lg-2">
+                            <a class="nav-link" aria-current="page" href="{{route('comics.index')}}"><span><i class="fa-solid fa-user-gear px-2"></i></span>ADMIN</a>
+                        </li>
+                    </ul>
+                </div>
+                <!-- /nav right -->
+        </nav>
     </div>
+    <!-- /container -->
 </header>
