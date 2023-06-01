a9f8943c47923b58818743d1f294762a89e7db34
giuseppesciacca <sciaccagi@gmail.com>
2023-06-01 12:26:54 +0200

    [add] single error on field + fix validate rules

--- resources/views/admin/comics/create.blade.php
+++ resources/views/admin/comics/create.blade.php
@@ -2,19 +2,19 @@
 
 @section('content')
 
-@if ($errors->any())
-<div class="alert alert-danger">
-    <ul>
-        @foreach ($errors->all() as $error)
-        <li>{{$error}}</li>
-        @endforeach
-    </ul>
-</div>
-@endif
-
 <div class="bg-light py-3">
 
     <div class="container">
+        @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                <li>{{$error}}</li>
+                @endforeach
+            </ul>
+        </div>
+        @endif
+
         <h5 class="text-uppercase text-muted my-4">Add a new Comic</h5>
 
         <form action="{{route('comics.store')}}" method="post">
@@ -23,30 +23,73 @@
             <div class="mb-3">
                 <label for="title" class="form-label">Title</label>
                 <input type="text" name="title" id="title" class="form-control" placeholder="Comic title here " aria-describedby="nameHelper">
+
+                @error('title')
+                <div class="alert alert-danger" role="alert">
+                    <strong>Errore: </strong>{{$message}}
+                </div>
+                @enderror
+
             </div>
             <div class="mb-3">
                 <label for="description" class="form-label">Description</label>
                 <input type="text" name="description" id="description" class="form-control" placeholder="Comic description here " aria-describedby="nameHelper">
+
+                @error('description')
+                <div class="alert alert-danger" role="alert">
+                    <strong>Errore: </strong>{{$message}}
+                </div>
+                @enderror
             </div>
             <div class="mb-3">
                 <label for="thumb" class="form-label">Image</label>
                 <input type="text" name="thumb" id="thumb" class="form-control" placeholder="Comic image here " aria-describedby="imageHelper">
+
+                @error('thumb')
+                <div class="alert alert-danger" role="alert">
+                    <strong>Errore: </strong>{{$message}}
+                </div>
+                @enderror
             </div>
             <div class="mb-3">
                 <label for="price" class="form-label">Price</label>
                 <input type="text" name="price" id="price" class="form-control" placeholder="Comic price here " aria-describedby="priceHelper">
+
+                @error('price')
+                <div class="alert alert-danger" role="alert">
+                    <strong>Errore: </strong>{{$message}}
+                </div>
+                @enderror
             </div>
             <div class="mb-3">
                 <label for="series" class="form-label">Series</label>
                 <input type="text" name="series" id="series" class="form-control" placeholder="Comic series here " aria-describedby="imageHelper">
+
+                @error('series')
+                <div class="alert alert-danger" role="alert">
+                    <strong>Errore: </strong>{{$message}}
+                </div>
+                @enderror
             </div>
             <div class="mb-3">
                 <label for="sale_date" class="form-label">Sale date</label>
                 <input type="text" name="sale_date" id="sale_date" class="form-control" placeholder="Comic sale date here " aria-describedby="imageHelper">
+
+                @error('sale_date')
+                <div class="alert alert-danger" role="alert">
+                    <strong>Errore: </strong>{{$message}}
+                </div>
+                @enderror
             </div>
             <div class="mb-3">
                 <label for="type" class="form-label">Type</label>
                 <input type="text" name="type" id="type" class="form-control" placeholder="Comic sale date here " aria-describedby="imageHelper">
+
+                @error('type')
+                <div class="alert alert-danger" role="alert">
+                    <strong>Errore: </strong>{{$message}}
+                </div>
+                @enderror
             </div>
 
             <button type="submit" class="btn btn-primary w-100 my-4">Save</button>
